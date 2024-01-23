19c144a9be17ef68ddacc9f6928fe9f430eab9c1
EstherSlabbert <118745552+EstherSlabbert@users.noreply.github.com>
2024-01-23 17:12:08 +0000

    Update game.js with play again functionality
    plus prepped for styling

--- game.js
+++ game.js
@@ -1,6 +1,7 @@
 const rockButton = document.getElementById('rock');
 const paperButton = document.getElementById('paper');
 const scissorsButton = document.getElementById('scissors');
+const playAgainButton = document.getElementById('again');
 
 const result = document.getElementById('result');
 const finalResult = document.getElementById('final');
@@ -50,6 +51,30 @@ function outcomeAndTally(playerSelection) {
     }
 }
 
+function playAgain() {
+    // Reset scores and display
+    playerScore = 0;
+    computerScore = 0;
+    ties = 0;
+    displayScores();
+
+    // Enable buttons again
+    gameContinues = true;
+
+    // Remove the last final result & round result
+    finalResult.innerHTML = '';
+    result.innerHTML = '';
+
+    // Add event listeners for the new round
+    if (gameContinues) {
+        again.style.display = 'none';
+        rockButton.addEventListener('click', rockButtonHandler);
+        paperButton.addEventListener('click', paperButtonHandler);
+        scissorsButton.addEventListener('click', scissorsButtonHandler);
+        playAgainButton.removeEventListener('click', playAgain);
+    }
+}
+
 const buttonHandler = (playerSelection) => {
     // the game is played, the outcome is determined and the scores are tallied
     outcomeAndTally(playerSelection);
@@ -77,9 +102,12 @@ function displayFinalResults() {
         finalResult.innerHTML = "You lost the game! Better luck next time!";
     }
 
+    again.style.display = 'block';
+
     rockButton.removeEventListener('click', rockButtonHandler);
     paperButton.removeEventListener('click', paperButtonHandler);
     scissorsButton.removeEventListener('click', scissorsButtonHandler);
+    playAgainButton.addEventListener('click', playAgain);
 }
 
 if (gameContinues) {
